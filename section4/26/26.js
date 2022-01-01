87e5b2d9b39fb0b0906e95d06f7e2fcfeb0afd13
fatihk29 <fatihkrc7@gmail.com>
2022-01-01 14:35:06 +0300

    files generated

---
+++ section4/26/26.js
@@ -0,0 +1,15 @@
+let str =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi minus, delectus aut doloremque ipsum rem labore officiis repellat corporis aliquam reprehenderit odit quos excepturi dolor. Autem architecto dolores deleniti esse';
+
+///// converting string to an array //////
+let strArray = [...str];
+let strArray2 = Array.from(str);
+
+console.log('strArray_6', strArray);
+console.log('strArray2_6', strArray2);
+///////////////////////////////////////////
+
+////// Array.isArray ///////
+let isArray = Array.isArray(strArray);
+console.log('isArray_10', isArray);
+////////////////////////////////
