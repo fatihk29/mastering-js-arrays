110b5462e452d277b1bb65b6130f4d4e72104396
fatihk29 <fatihkrc7@gmail.com>
2022-01-12 19:44:26 +0300

    new sections added

--- section3/15-16-17/index.js
+++ section3/15-16-17/index.js
@@ -7,3 +7,16 @@ let array1 = [10, 11, 12, 13, 14, 15, 16, 17, 18];
 for (let i = 0; i < array1.length; i += 1) {
   console.log("element", i, "=>", array1[i]);
 }
+
+// Exercise 2 Question 22
+
+let students = ["Ahmet", "John", "Mert", "Ayse", "Zeynep", "haydar", "bengu"];
+
+let studentsFormatted = [];
+// Exercise 2 Solving 2
+for (let i = 0; i < students.length; i += 1) {
+  // studentsFormatted.push(students[i].toLocaleLowerCase());
+  studentsFormatted[i] = students[i].toLocaleLowerCase();
+}
+
+console.log("studentsFormatted", studentsFormatted);
---
+++ section3/20-21/index.js
@@ -0,0 +1,13 @@
+// for in loop
+
+let array1 = [3, 5, 7, 9, 11, 13, 15, 17, 18, 19, 20, 21];
+
+for (let key in array1) {
+  console.log("key", key);
+}
+
+// for of loop
+
+for (let value of array1) {
+  console.log("value", value);
+}
